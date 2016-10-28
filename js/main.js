873bd9d3459d617e3beb08d069bfea82f4def0f9
Jsmithseo <Jeremysmithseo@gmail.com>
2016-10-28 16:12:18 -0700

    thirdCommit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pauseOnHover: true,
+    interval: 5000,
+  });
+    });
